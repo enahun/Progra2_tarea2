9bcfecd72bda2dea59ddec8eb1ecaec7da4d61e7
enahun <nahunecheverria@unitec.edu>
2015-02-15 17:18:24 -0600

    Update Rectangulo.java

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,34 +1,41 @@
 
 public class Rectangulo {
-	//Atributos
-int base, altura;
+	//Atributos de la clase
+	int base, altura;
 	
+	//Se crea constructor de la clase Rectangulo
+	//Se asignan los valores a base y altura
 	Rectangulo(int base, int altura)
 	{
 		this.base=base;
 		this.altura=altura;
 	}
-	
+	//Retorno el valor del atributo base
 	int getBase()
 	{
 		return base;
 	}
+	//Inicia metodo set base
 	void setBase(int base)
 	{
 		this.base=base;
 	}
+	//Retorno valor del atributo altura
 	int getAltura()
 	{
 		return altura;
 	}
+	//Inicia metodo set altura
 	void setAltura(int altura)
 	{
 		this.altura=altura;
 	}
+	//metodo retorna el calculo de la area
 	int getArea()
 	{
 		return (base * altura);
 	}
+	//Metodo retorna el calculo del perimetro
 	int getPerimetro()
 	{
 		return 2*(base+altura);
